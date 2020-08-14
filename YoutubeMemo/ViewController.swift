37edfe7cc47bcd5c5a18ca1314403f2f5aeb6246
Yoshinori Kobayashi <yoshinori.kobayashi@music.yamaha.com>
2020-08-14 23:48:30 +0900

    プロジェクト追加

---
+++ YoutubeMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeMemo
+//
+//  Created by yoshiiikoba on 2020/08/14.
+//  Copyright © 2020 Kobayashi Yoshinori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
